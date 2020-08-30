1ada71e811c726066b06a05fa15cbd254f80c9fa
manojlkkhadka <manojlkkhadka@gmail.com>
2020-08-30 13:28:50 +0545

    Updates

---
+++ precache-manifest.6a3e11796afcccde0c983e0213de2762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde5c25014d624de10a75bd304c4a789",
+    "url": "/color-generator-react/index.html"
+  },
+  {
+    "revision": "c6a43789448045b3bfa1",
+    "url": "/color-generator-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cfc2e6b7a7662c48ae3",
+    "url": "/color-generator-react/static/js/2.851e79d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-generator-react/static/js/2.851e79d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a43789448045b3bfa1",
+    "url": "/color-generator-react/static/js/main.9e182374.chunk.js"
+  },
+  {
+    "revision": "0501ade12effb86e0486",
+    "url": "/color-generator-react/static/js/runtime-main.93c07835.js"
+  }
+]);
